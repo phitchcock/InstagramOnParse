7db3ec72cf0144beb70717b4e063147be6df1c52
phitchcock <phitchcock@me.com>
2015-01-13 09:40:34 -0800

    Initial Commit

---
+++ InstagramOnParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramOnParse
+//
+//  Created by Peter Hitchcock on 1/13/15.
+//  Copyright (c) 2015 Peter Hitchcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
